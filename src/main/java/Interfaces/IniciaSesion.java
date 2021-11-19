1032564847388ead6afa7c72b43f5afef66891fe
Ericsson715 <92638586+Ericsson715@users.noreply.github.com>
2021-11-19 14:30:43 -0600

    cambio del boton y regreso de texto con el titulo "SALIR"

--- src/main/java/Interfaces/IniciaSesion.java
+++ src/main/java/Interfaces/IniciaSesion.java
@@ -48,7 +48,7 @@ public class IniciaSesion extends javax.swing.JFrame {
         });
 
         salir.setForeground(new java.awt.Color(153, 0, 0));
-        salir.setText("POR FIN CREO QUE SALIO");
+        salir.setText("SALIR");
 
         iconoUsuario.setBackground(new java.awt.Color(102, 102, 255));
 
